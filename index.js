5d058baf6216ad2fce53bff9da619fbd476852b6
MII-dev <igor.malyarenko19@gmail.com>
2023-07-25 19:14:50 +0300

    Type Conversion

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-//let usename = window.prompt("What`s youe name?")
-//console.log(usename)
+//let age = window.prompt("how old are you?")
 
-let username
+//console.log(typeof age)
+//age = Number(age)
+//age += 1
 
-document.getElementById("myButton").onclick = function(){
-    username = document.getElementById("myText").value
-    console.log(username)
-    document.getElementById("myLabel").innerHTML = "Hello " + username
-}
+//console.log("Happe Birthday! You are", age, "years old")
+
+
+let x
+let y 
+let z
+
+x = Number("3.14")
+y = String(3.14)
+z = Boolean("pizza")
+console.log(x, typeof x)
+console.log(y, typeof y)
+console.log(z, typeof z)
+
+
