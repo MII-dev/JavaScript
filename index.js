6057c36ed5d8e56e1729a1f5b132e274202c1215
MII-dev <igor.malyarenko19@gmail.com>
2023-07-27 17:37:31 +0300

    1st

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-//let age = window.prompt("how old are you?")
-
-//console.log(typeof age)
-//age = Number(age)
-//age += 1
-
-//console.log("Happe Birthday! You are", age, "years old")
-
-
 let x
-let y 
-let z
+let y = 5
+let z = 9
+let maximum
+let minimum
 
-x = Number("3.14")
-y = String(3.14)
-z = Boolean("pizza")
-console.log(x, typeof x)
-console.log(y, typeof y)
-console.log(z, typeof z)
+//x = Math.round(x) // = 3
+//x = Math.floor(x) // = 3
+//x = Math.ceil(x)  // = 4
+//x = Math.pow(x,2) // = 9.8596
+//x = Math.sqrt(x)  // = 1.77
+//x = Math.abs(x)   // = positive x
 
+//maximum = Math.max(x,y,z)   // = 9
+//minimum = Math.min(x,y,z)   // = 3.14
 
+//x = Math.PI         // = 3.141592653589793
+
+console.log(x)
+console.log(maximum)
+console.log(minimum)
