621fecf0cdd2cfc04be09c4cfe7a7abf3381327f
MII-dev <igor.malyarenko19@gmail.com>
2023-07-28 18:59:44 +0300

    1st

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
 
-console.log("happy birthday to you!")
-console.log("happy birthday to you!")
-console.log("happy birthday dear ____!")
-console.log("happy birthday to you!")
-console.log("You are ___ years old!")
+checkWinner(true)
 
-function startProgram(){
-    let userName = "Ihor"
-    let age = 24
-    happyBirthday(userName,age)
-    happyBirthday(userName,age)
-
+function checkWinner(win){
+    win         ? console.log("You win!")   : console.log("You Lose!")
+//  condition   ? exprIfTrue                : exprIfFalse
 }
 
-function happyBirthday(userName, age){
-    console.log("happy birthday to you!")
-    console.log("happy birthday to you!")
-    console.log("happy birthday dear", userName + "!")
-    console.log("happy birthday to you!")
-    console.log("You are", age ,"years old!")
-}
 
-startProgram()
