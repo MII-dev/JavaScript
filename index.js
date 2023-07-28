9ad21ba6761b01f726fc14155a3d5c1770c0eb15
MII-dev <igor.malyarenko19@gmail.com>
2023-07-28 18:28:18 +0300

    1st

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
-let grade = 59
 
-switch(true){
 
-    case grade >= 90:
-        console.log("You did great!")
-        break
-    case grade >= 80:
-        console.log("You did good!")
-        break
-    case grade >= 70:
-        console.log("You did okay!")
-        break
-    case grade >= 60:
-        console.log("You passed ... barely!")
-        break    
-    case grade < 60:
-        console.log("You FAILED!")
-        break
-    default:
-        console.log(grade, "is not a letter grade")
-    
-}
+let userName
+
+do{
+    userName = window.prompt("Enter your name")
+}while(userName == "" || userName == null)
+
+console.log("Hello", userName)
