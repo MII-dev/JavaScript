de774e926bff66291e3934e2d19e3201d1e11219
MII-dev <igor.malyarenko19@gmail.com>
2023-07-31 18:28:34 +0300

    1st

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 
-checkWinner(true)
+let userName = "Ihor"
+let items = 3
+let total = 75
 
-function checkWinner(win){
-    win         ? console.log("You win!")   : console.log("You Lose!")
-//  condition   ? exprIfTrue                : exprIfFalse
-}
+console.log("Hello", userName)
+console.log("You have", items, "items in your cart")
+console.log("Your total is: $", total)
 
+console.log(`Hello ${userName}`)
+console.log(`You have ${items} items in your cart`)
+console.log(`Your total is $${total} `)
 
+let text = 
+`Hello ${userName}<br>
+You have ${items} items in your cart<br>
+Your total is $${total} <br>`
+
+console.log(text)
+
+document.getElementById("myLabel").innerHTML = text
