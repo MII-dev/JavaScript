aea702bef5b85dac78935872fb01b01f994680bc
MII-dev <igor.malyarenko19@gmail.com>
2023-07-31 19:06:47 +0300

    1st

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 
+document.getElementById("submitButton").onclick = function() {
+
+    let temp
+
+    if (document.getElementById("cButton").checked) {
+
+        temp = document.getElementById("textBox").value
+        temp = Number(temp)
+        temp = toCelsius(temp)
+        document.getElementById("tempLabel").innerHTML = temp + "°C"
+
+    } else if (document.getElementById("fButton").checked) {
+
+        temp = document.getElementById("textBox").value
+        temp = Number(temp)
+        temp = toFahrenheit(temp)
+        document.getElementById("tempLabel").innerHTML = temp + "°F"
+
+    } else {
+        document.getElementById("tempLabel").innerHTML = "Select a unit"
+    }
 
 
+
+
+}
 
 
 let temp = 32
