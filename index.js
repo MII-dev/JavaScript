eac6c415dada11a17bbd513e8c0b7367b32df9eb
MII-dev <igor.malyarenko19@gmail.com>
2023-07-31 18:37:20 +0300

    1st

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
+ let myNum = 123456.789
 
-let userName = "Ihor"
-let items = 3
-let total = 75
+ myNum = myNum.toLocaleString("en-US")      //  US English
 
-console.log("Hello", userName)
-console.log("You have", items, "items in your cart")
-console.log("Your total is: $", total)
+ console.log(myNum)
 
-console.log(`Hello ${userName}`)
-console.log(`You have ${items} items in your cart`)
-console.log(`Your total is $${total} `)
+ myNum = 123456.789
 
-let text = 
-`Hello ${userName}<br>
-You have ${items} items in your cart<br>
-Your total is $${total} <br>`
+ myNum = myNum.toLocaleString("hi-IN")      //  Hindi
 
-console.log(text)
+ console.log(myNum)
 
-document.getElementById("myLabel").innerHTML = text
+ myNum = 123456.789
+
+ myNum = myNum.toLocaleString("de-DE")      //  standart German
+
+ console.log(myNum)
+
+ myNum = 123456.789
+
+ myNum = myNum.toLocaleString("en-US", {style: "currency", currency: "USD"})      //  US English
+
+ console.log(myNum)
+
+ myNum = 123456.789
+
+ myNum = myNum.toLocaleString("hi-IN", {style: "currency", currency: "INR"})      //  US English
+
+ console.log(myNum)
+
+ myNum = 123456.789
+
+ myNum = myNum.toLocaleString("de-DE", {style: "currency", currency: "EUR"})      
+
+ console.log(myNum)
+
+ myNum = .5
+
+ myNum = myNum.toLocaleString(undefined, {style: "percent"})
+
+ console.log(myNum)
+
+ myNum = 100
+
+ myNum = myNum.toLocaleString(undefined, {style: "unit", unit: "celsius"})
+
+ console.log(myNum)
+
