92b7cd9bdcec129acb94ae2b5bf407ca9046ff6d
MII-dev <igor.malyarenko19@gmail.com>
2023-07-31 18:48:01 +0300

    1st

--- index.js
+++ index.js
@@ -1,48 +1,21 @@
- let myNum = 123456.789
 
- myNum = myNum.toLocaleString("en-US")      //  US English
+const answer = Math.floor(Math.random() * 10 + 1)
+let guesses = 0
 
- console.log(myNum)
+document.getElementById("submitButton").onclick = function(){
 
- myNum = 123456.789
+    let guess = document.getElementById("guessField").value
 
- myNum = myNum.toLocaleString("hi-IN")      //  Hindi
+    guesses++
 
- console.log(myNum)
+    if(guess == answer){
+        alert(`${answer} is the #. It took you ${guesses} guesses`)
+    }
+    else if(guess < answer){
+        alert("Too small!")
+    }
+    else{
+        alert("Too large!")
+    }
 
- myNum = 123456.789
-
- myNum = myNum.toLocaleString("de-DE")      //  standart German
-
- console.log(myNum)
-
- myNum = 123456.789
-
- myNum = myNum.toLocaleString("en-US", {style: "currency", currency: "USD"})      //  US English
-
- console.log(myNum)
-
- myNum = 123456.789
-
- myNum = myNum.toLocaleString("hi-IN", {style: "currency", currency: "INR"})      //  US English
-
- console.log(myNum)
-
- myNum = 123456.789
-
- myNum = myNum.toLocaleString("de-DE", {style: "currency", currency: "EUR"})      
-
- console.log(myNum)
-
- myNum = .5
-
- myNum = myNum.toLocaleString(undefined, {style: "percent"})
-
- console.log(myNum)
-
- myNum = 100
-
- myNum = myNum.toLocaleString(undefined, {style: "unit", unit: "celsius"})
-
- console.log(myNum)
-
+}
