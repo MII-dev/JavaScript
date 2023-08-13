c601923490bb5a8a48d6b2d876a52e149780b240
MII-dev <igor.malyarenko19@gmail.com>
2023-08-13 13:26:45 +0300

    1st

--- index.js
+++ index.js
@@ -1,40 +1,22 @@
 
+let cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+shuffle(cards)
+
+cards.forEach(card => console.log(card))
 
 
-const greeting = function(userName){
-    console.log(`Hello ${userName}`)
+function shuffle(array){
+    let currentIndex = array.length
+
+    while(currentIndex != 0){
+        let randomIndex = Math.floor(Math.random() * array.length)
+        currentIndex -= 1
+
+        let temp = array[currentIndex]
+        array[currentIndex] = array[randomIndex]
+        array[randomIndex] = temp
+    }
+
+    return array
 }
-
-greeting("Ihor")
-
-const arrowGreeting = (userName) => console.log(`Hello ${userName}`)
-
-arrowGreeting("Ihor (arrow func)")
-
-
-
-
-
-const percent = function(x, y){
-    return x / y * 100
-}
-
-console.log(`${percent(75, 100)}%`)
-
-const percentArrow = (x, y) =>  x / y * 100
-
-console.log(`${percentArrow(75, 100)}% (arrow func)`)
-
-
-
-
-
-
-let grades = [100, 50, 90, 60, 80, 70]
-
-grades.sort( (x, y) => y - x)
-grades.forEach((element) => console.log(element))
-
-
-
-
