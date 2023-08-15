2a62e8f2bcd0597a877d70782c516f9ff1d34329
MII-dev <igor.malyarenko19@gmail.com>
2023-08-15 11:36:50 +0300

    1st

--- index.js
+++ index.js
@@ -1,40 +1,33 @@
 
-class Car{
-
-    constructor(model, year, color){
-        this.model = model
-        this.year = year
-        this.color = color
-    }
-
-    drive(){
-        console.log(`You drive the ${this.model}`)
+class Card{
+    constructor(value, suit){
+        this.value = value
+        this.suit = suit
     }
 }
 
-const car1 = new Car("Mustang", 2023, "red")
-const car2 = new Car("Corvette", 2024, "blue")
-const car3 = new Car("Lambo", 2022, "yellow")
-const car4 = new Car("Ferrari", 2025, "White")
 
+/*
+let card1 = new Card("A", "Hearts")
+let card2 = new Card("A", "Spades")
+let card3 = new Card("A", "Diamonds")
+let card4 = new Card("A", "Clubs")
+let card5 = new Card("2", "Hearts")
+let card6 = new Card("2", "Spades")
+let card7 = new Card("2", "Diamonds")
+let card8 = new Card("2", "Clubs")
 
-const cars = [car1, car2, car3, car4]
+let cards = [card1, card2, card3, card4, card5, card6, card7, card8]
+*/
 
-console.log(cars[0].model)
-console.log(cars[1].model)
-console.log(cars[2].model)
+let cards = [new Card("A", "Hearts"),
+             new Card("A", "Spades"),
+             new Card("A", "Diamonds"),
+             new Card("A", "Clubs"),
+             new Card("2", "Hearts"),
+             new Card("2", "Spades"),
+             new Card("2", "Diamonds"),
+             new Card("2", "Clubs")]
 
-cars[0].drive()
-cars[1].drive()
-cars[2].drive()
-
-startRace(cars)
-
-function startRace(cars){
-
-    for(const car of cars){
-        car.drive()
-    }
-
-}
+cards.forEach(card => console.log(`${card.value} ${card.suit}`))
 
