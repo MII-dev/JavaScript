139de03a6a609101ef1bb030923ffd8646447a84
MII-dev <igor.malyarenko19@gmail.com>
2023-08-15 11:57:53 +0300

    1st

--- index.js
+++ index.js
@@ -1,27 +1,14 @@
 
+let count = 0
+let max = window.prompt("Count up to what #?")
+max = Number(max)
 
+const myTimer = setInterval(countUp,1000)
 
-let item = "cryptocurrency"
-let price = 420.29
-
-let timer1 = setTimeout(firstMessage, 3000, item, price)
-let timer2 = setTimeout(secondMessage, 6000)
-let timer3 = setTimeout(thirdMessage, 9000)
-
-function firstMessage(item, price){
-    alert(`Buy this ${item} for $${price}`)
-}
-function secondMessage(){
-    alert(`This not a scam)`)
-}
-function thirdMessage(){
-    alert(`!!DO IT!!`)
-}
-
-
-document.getElementById("myButton").onclick = function(){
-    clearTimeout(timer1)
-    clearTimeout(timer2)
-    clearTimeout(timer3)
-    alert(`Thanks for buying <3`)
+function countUp(){
+    count++
+    console.log(count)
+    if(count >= max){
+        clearInterval(myTimer)
+    }
 }
